86672938b7294ac9d3086629d14b96e49dd1bce3
mochimoko <jerez.solis@gmail.com>
2021-02-12 00:30:18 -0800

    added files

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import './Home.css';
+import amazonHero from './images/amazon_hero.jpg'
 
 const Home = () => {
   return (
-    <div>
-      
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src={amazonHero}
+          alt="amazon body background "
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
