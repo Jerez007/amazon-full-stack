38d87c3fe096ee7dcf79b27b38e13c4c4395aee4
mochimoko <jerez.solis@gmail.com>
2021-02-12 10:20:58 -0800

    added files

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -10,18 +10,24 @@ const Home = () => {
         <img className="home__image" src={amazonHero} alt="amazon hero" />
 
         <div className="home__row">
+          <Product
+            title=" The Lean Startup: How Today's Entrepreneurs Use Continuous Innovation
+          to Create Radically Successful Businesses"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"
+            rating={3}
+          />
           <Product />
-          {/* Product */}
         </div>
 
         <div className="home__row">
-          {/* Product */}
-          {/* Product */}
-          {/* Product */}
+          <Product />
+          <Product />
+          <Product />
         </div>
 
         <div className="home__row">
-          {/* Product */}
+          <Product />
         </div>
       </div>
     </div>
--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -1,28 +1,26 @@
 import React from 'react'
 import './Product.css';
 
-function Product() {
+function Product({ title, image, price, rating }) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>
-          The Lean Startup: How Today's Entrepreneurs Use Continuous Innovation
-          to Create Radically Successful Businesses
-        </p>
+        <p>{title}</p>
         <p className="product__price">
           <small>$</small>
-          <strong>19.99</strong>
+          <strong>{price}</strong>
         </p>
 
         <div className="product__rating">
-          <p>⭐</p>
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
         </div>
       </div>
 
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"
-        alt="lean startup book cover"
-      />
+      <img src={image} alt="lean startup book cover" />
 
       <button>Add to Basket</button>
     </div>
