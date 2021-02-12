0145a989c549c577e3fb7ad3dab82420e40c7c8f
mochimoko <jerez.solis@gmail.com>
2021-02-12 00:15:04 -0800

    added files

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Amazon Clone</h1>
-
-      {/* Header */}
+      <Header />
+      <Home />
       {/* Home */}
       
     </div>
--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Header.css";
 import logo from "./images/amazon_logo.png";
+import SearchIcon from '@material-ui/icons/Search';  
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 function Header() {
   return (
@@ -8,15 +10,29 @@ function Header() {
       <img src={logo} alt="" className="header__logo" />
       <div className="header__search">
         <input type="text" className="header__searchInput" />
-        {/* Logo */}
+        <SearchIcon className='header__searchIcon'/>
       </div>
 
       <div className="header__nav">
-        <div className="header__option"></div>
+        <div className="header__option">
+          <span className="option__headerLineOne">Hello Guest</span>
+          <span className="option__headerLineTwo">Sign In</span>
+        </div>
 
-        <div className="header__option"></div>
+        <div className="header__option">
+          <span className="option__headerLineOne">Returns</span>
+          <span className="option__headerLineTwo">& Orders</span>
+        </div>
 
-        <div className="header__option"></div>
+        <div className="header__option">
+          <span className="option__headerLineOne">Your</span>
+          <span className="option__headerLineTwo">Prime</span>
+        </div>
+
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header__optionLineTwo header__basketCount">0</span>
+        </div>
       </div>
     </div>
   );
