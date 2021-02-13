cd9a3c6b144a2d61deed84acc05c9f8ce4d6704e
mochimoko <jerez.solis@gmail.com>
2021-02-13 11:35:55 -0800

    added more files

--- amazon-clone/src/Login.js
+++ amazon-clone/src/Login.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Login.css";
 
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState("");
+
+  const signIn = (e) => {
+    e.preventDefault(); //prevents refreshing
+
+    //firebase
+  };
+
+  const register = (e) => {
+    e.preventDefault(); //prevents refreshing
+
+    //firebase
+  };
+
   return (
     <div className="login">
       <Link to="/">
@@ -18,21 +33,39 @@ function Login() {
         <h1>Sign-in</h1>
         <form action="">
           <h5>E-mail</h5>
-          <input type="text" />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
           <h5>Password</h5>
-          <input type="password" />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-          <button className="login__signInButton">Sign In</button>
+          <button
+            type="submit"
+            onClick={signIn}
+            className="login__signInButton"
+          >
+            Sign In
+          </button>
         </form>
 
         <p>
-          By continuing, you agree to FAKE Amazon CLONE{" "}
+          By continuing, you agree to FAKE Amazon CLONE
           <a href="#">Conditions of Use</a>
           and <a href="#">Privacy Notice</a>
         </p>
 
-        <button className="login__registerButton">
+        <button
+          type="submit"
+          onClick={register}
+          className="login__registerButton"
+        >
           Create your Amazon account
         </button>
       </div>
--- amazon-clone/src/firebase.js
+++ amazon-clone/src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
   apiKey: "AIzaSyAbX6AVGDTZyp3SvrvkAkwkCjLm7TTVhgM",
   authDomain: "clone-b037a.firebaseapp.com",
@@ -6,3 +8,11 @@ const firebaseConfig = {
   messagingSenderId: "139081678451",
   appId: "1:139081678451:web:9ebfad75478c1906223406",
 };
+
+
+const firebaseApp = firebase.initiliazeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
