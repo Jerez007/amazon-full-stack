cb2ccb1fe795706cf81f6442867ddb1d0a88dc01
mochimoko <jerez.solis@gmail.com>
2021-02-13 12:49:52 -0800

    added more files

--- amazon-clone/src/Checkout.js
+++ amazon-clone/src/Checkout.js
@@ -5,7 +5,7 @@ import { useStateValue } from "./StateProvider";
 import Subtotal from "./Subtotal";
 
 function Checkout() {
-  const [{ basket }] = useStateValue();
+  const [{ basket, user }] = useStateValue();
 
 
   return (
@@ -18,6 +18,7 @@ function Checkout() {
         />
 
         <div>
+          <h3>{user ? 'Hello, ' + user.email : ''}</h3>
           <h2 className="checkout__title">Your shopping Basket</h2>
 
           {basket.map((item) => (
